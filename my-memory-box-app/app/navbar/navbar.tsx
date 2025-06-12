14127a9e00f936fe74e0983efa318f1c98898576
saha-7op <saha7pritam@gmail.com>
2025-06-12 22:15:58 +0530

    Sign in sign out functionality added

--- my-memory-box-app/app/navbar/navbar.tsx
+++ my-memory-box-app/app/navbar/navbar.tsx
@@ -3,20 +3,34 @@
 import Image from "next/image"
 import Link from "next/link"
 import SignIn from "./signin"
+import { useEffect, useState } from "react"
+import { onAuthStateChangedHelper } from "../firebase/firebase"
+import { User } from "firebase/auth"
 
 export default function Navbar() {
+    // Initialized user state
+    const [user, setUser] = useState<User | null>(null)
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChangedHelper((user)=>{
+            setUser(user)
+        })
+        // cleanup subscription on unmount
+        return () => unsubscribe()
+    }, [])
+
     return(
-        <nav className="flex items-center justify-between bg-gradient-to-b from-blue-500 to-blue-700 px-4 py-2 ">
+        <nav className="flex items-center justify-between bg-gradient-to-l from-gray-800 to-gray-300 px-4 py-2 shadow-lg shadow-black ">
             <Link href="/" className="flex items-center cursor-pointer">
             <Image 
                 src="/logo_mb.png" 
                 alt="logo" 
                 width={60}  
                 height={60} 
-                className="w-20 h-20 rounded-full" 
+                className="w-16 h-16 rounded-full" 
             />
             </Link>
-            <SignIn/>
+            <SignIn user={user}/>
         </nav>
     )
 }
--- my-memory-box-app/app/navbar/signin.tsx
+++ my-memory-box-app/app/navbar/signin.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { signInWithGoogle, signOut } from '../firebase/firebase'
+import { User } from 'firebase/auth'
 
-const SignIn = () => {
+interface SignInProps{
+  user: User | null
+}
+const SignIn = ({user}: SignInProps) => {
   return (
     <>
-    <button onClick={signOut} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
+    { user ? (
+      <button onClick={signOut} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ">
       Sign Out
     </button>
-    <button onClick={signInWithGoogle} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ml-2">
+    ) : (
+      <button onClick={signInWithGoogle} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ml-2">
       Sign In
     </button>
+    )
+  }
     </>
   )
 }
