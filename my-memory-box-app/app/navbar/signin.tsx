4d26c541db8e523f41337b075fcc27c04c7ae262
saha-7op <saha7pritam@gmail.com>
2025-06-12 22:29:02 +0530

    button style

--- my-memory-box-app/app/navbar/signin.tsx
+++ my-memory-box-app/app/navbar/signin.tsx
@@ -9,7 +9,7 @@ const SignIn = ({user}: SignInProps) => {
   return (
     <>
     { user ? (
-      <button onClick={signOut} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ">
+      <button onClick={signOut} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300">
       Sign Out
     </button>
     ) : (
