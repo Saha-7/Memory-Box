8b558e04e63acf2309d53b9020df6bcb1eb2fc8e
saha-7op <saha7pritam@gmail.com>
2025-06-12 22:31:24 +0530

    Button positioning

--- my-memory-box-app/app/navbar/signin.tsx
+++ my-memory-box-app/app/navbar/signin.tsx
@@ -9,11 +9,11 @@ const SignIn = ({user}: SignInProps) => {
   return (
     <>
     { user ? (
-      <button onClick={signOut} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300">
+      <button onClick={signOut} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300 cursor-pointer mr-2">
       Sign Out
     </button>
     ) : (
-      <button onClick={signInWithGoogle} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ml-2">
+      <button onClick={signInWithGoogle} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300 cursor-pointer mr-2">
       Sign In
     </button>
     )
