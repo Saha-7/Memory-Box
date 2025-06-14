354a0932328f019d9df9d80fc7fb9939c423b02a
saha-7op <saha7pritam@gmail.com>
2025-06-14 09:32:30 +0530

    Add Create user function & Add video upload functions

--- mb-api-service/functions/.eslintrc.js
+++ mb-api-service/functions/.eslintrc.js
@@ -1,3 +1,38 @@
+// module.exports = {
+//   root: true,
+//   env: {
+//     es6: true,
+//     node: true,
+//   },
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:import/errors",
+//     "plugin:import/warnings",
+//     "plugin:import/typescript",
+//     "google",
+//     "plugin:@typescript-eslint/recommended",
+//   ],
+//   parser: "@typescript-eslint/parser",
+//   parserOptions: {
+//     project: ["tsconfig.json", "tsconfig.dev.json"],
+//     sourceType: "module",
+//   },
+//   ignorePatterns: [
+//     "/lib/**/*", // Ignore built files.
+//     "/generated/**/*", // Ignore generated files.
+//   ],
+//   plugins: [
+//     "@typescript-eslint",
+//     "import",
+//   ],
+//   rules: {
+//     "quotes": ["error", "double"],
+//     "import/no-unresolved": 0,
+//     "indent": ["error", 2],
+//   },
+// };
+
+
 module.exports = {
   root: true,
   env: {
@@ -6,28 +41,26 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
     "google",
-    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
+    ecmaVersion: 2020,
     sourceType: "module",
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
+    "/lib/**/*",
+    "/node_modules/**/*",
   ],
   plugins: [
     "@typescript-eslint",
-    "import",
   ],
   rules: {
     "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "max-len": ["error", { "code": 100 }],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "object-curly-spacing": ["error", "always"],
   },
 };
--- mb-api-service/functions/src/index.ts
+++ mb-api-service/functions/src/index.ts
@@ -1,19 +1,84 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
+import * as functions from "firebase-functions";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import { Storage } from "@google-cloud/storage";
+import { onCall } from "firebase-functions/v2/https";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+initializeApp();
+const firestore = getFirestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "demo-raw-videos";
+
+export const createUser = functions.auth.user().onCreate(async (user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+    createdAt: new Date().toISOString(),
+  };
+
+  try {
+    await firestore
+      .collection("users")
+      .doc(user.uid)
+      .set(userInfo);
+
+    logger.info(`User created: ${JSON.stringify(userInfo)}`);
+  } catch (error) {
+    logger.error(`Error creating user: ${error}`);
+    throw error;
+  }
+});
+
+export const generateUploadUrl = onCall({ maxInstances: 1 }, async (request) => {
+  // Check if the user is authentication
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated.",
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return { url, fileName };
+});
+
+// import * as functions from "firebase-functions";
+// import { initializeApp } from "firebase-admin/app";
+// import { getFirestore } from "firebase-admin/firestore";
+// import * as logger from "firebase-functions/logger";
+
+// initializeApp();
+// const firestore = getFirestore();
+
+// export const createUser = functions.auth.user().onCreate((user) => {
+//   const userInfo = {
+//     uid: user.uid,
+//     email: user.email,
+//     photoUrl: user.photoURL,
+//   };
+
+//   return firestore
+//     .collection("users")
+//     .doc(user.uid)
+//     .set(userInfo)
+//     .then(() => {
+//       logger.info(`User created: ${JSON.stringify(userInfo)}`);
+//     });
 // });
